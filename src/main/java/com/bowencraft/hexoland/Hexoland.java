d82bfaa203d885256525d1fed0e16070901e288b
Bowen <1758364810@qq.com>
2022-09-05 14:01:19 -0400

    Created the project

---
+++ src/main/java/com/bowencraft/hexoland/Hexoland.java
@@ -0,0 +1,17 @@
+package com.bowencraft.hexoland;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Hexoland extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        
+    }
+    
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
